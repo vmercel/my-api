2e2335991023cdf987357f1dc2030b783f897019
Vubangsi Mercel <vmercel@gmail.com>
2022-08-06 12:38:29 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const app = express();
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+  });
+
 const savePost = () => {
   fs.writeFile(
     "./database/blogPosts.json",
